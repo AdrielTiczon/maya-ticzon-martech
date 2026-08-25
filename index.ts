5953a8158706e6c382359bffe4a2c33dbf4810f3
AdrielTiczon <adrielticzon@gmail.com>
2026-08-26 00:09:07 +0800

    lint: fix indentation

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-import express, { type Express, type Request, type Response } from "express";
+import app from "./src/app.ts";
 
-const app: Express = express();
-
-app.get("/health", (req: Request, res: Response) => {
-	console.info("[INFO] ENDPOINT: `/health`", { health: "OK" });
-	res.status(200).send("OK");
-});
-
-app.listen(3000, () => {
-	console.log("Remittance server status: STARTED");
+app.listen(process.env.PORT || 3000, () => {
+  console.info("[INFO] Remittance Server started");
 });
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { type Express, type Request, type Response } from "express";
+
+import usersController from "./controllers/users.controller.ts";
+import {
+	apiKeyAuthMiddleware,
+	errorHandlerMiddleware,
+	emptyRoutesMiddleware,
+} from "./middlewares/index.ts";
+
+const app: Express = express();
+
+// -- Middleware --
+app.use(express.json()); // Used for POST requests that utilizes body
+
+app.get("/health", (req: Request, res: Response) => {
+	console.info("[INFO] ENDPOINT: `/health`", { health: "OK" });
+	res.status(200).send("OK");
+});
+
+app.use("/users", apiKeyAuthMiddleware, usersController);
+// TODO: auth
+// TODO: remittance controller
+// app.use("/remittance", jwtAuthMiddleware, remittanceController)
+
+// -- Error Middlewares --
+app.use(emptyRoutesMiddleware); // Handles routes that do not exist
+app.use(errorHandlerMiddleware); // Handles error catching
+
+export default app;
