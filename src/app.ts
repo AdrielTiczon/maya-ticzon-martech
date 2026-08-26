157f82f6fc1cdba6de9264714656fc63cde9d5ef
AdrielTiczon <adrielticzon@gmail.com>
2026-08-26 14:16:13 +0800

    feat: add auth

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { type Express, type Request, type Response } from "express";
 import swaggerUi from "swagger-ui-express";
+import cookieParser from "cookie-parser";
 
 import openApiSpec from "../docs/openapi.json" with { type: "json" };
 
@@ -9,18 +10,24 @@ import {
   errorHandlerMiddleware,
   emptyRoutesMiddleware,
 } from "./middlewares/index.ts";
+import authController from "./controllers/auth.controller.ts";
+import transactionsController from "./controllers/transactions.controller.ts";
+import jwtAuthMiddleware from "./middlewares/auth.middleware.ts";
 
 const app: Express = express();
 
 // -- Middleware --
 app.use(express.json()); // Used for POST requests that utilizes body
+app.use(cookieParser());
 
 app.get("/health", (req: Request, res: Response) => {
   console.info("[INFO] ENDPOINT: `/health`", { health: "OK" });
   res.status(200).send("OK");
 });
 
+app.use("/auth", apiKeyAuthMiddleware, authController);
 app.use("/users", apiKeyAuthMiddleware, usersController);
+app.use("/transactions", jwtAuthMiddleware, transactionsController);
 // TODO: auth
 // TODO: remittance controller
 // app.use("/remittance", jwtAuthMiddleware, remittanceController)
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,13 +1,29 @@
 import type { NextFunction, Request, Response } from "express";
+import { verifyAccessToken } from "../services/token.service.ts";
+import { unauthorized } from "../utils/errors.ts";
 
 export default function jwtAuthMiddleware(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  console.info("TODO: auth middleware");
+  const token = req.cookies?.access_token;
+  if (!token)
+    throw unauthorized(
+      "UNAUTHORIZED",
+      "You are not allowed to access this resource.",
+    );
 
-  next();
+  try {
+    const payload = verifyAccessToken(token);
+    req.userId = payload.sub;
+    next();
+  } catch {
+    throw unauthorized(
+      "UNAUTHORIZED",
+      "You are not allowed to access this resource.",
+    );
+  }
 }
 
 export function apiKeyAuthMiddleware(
@@ -18,7 +34,10 @@ export function apiKeyAuthMiddleware(
   const apiKey = req.header("x-api-key");
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    throw new Error("UNAUTHORIZED");
+    throw unauthorized(
+      "UNAUTHORIZED",
+      "You are not allowed to access this resource.",
+    );
   }
 
   next();
