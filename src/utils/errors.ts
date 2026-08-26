a0c46356664d940d0c153c86f81eab0f7b664006
AdrielTiczon <adrielticzon@gmail.com>
2026-08-26 14:16:21 +0800

    feat: add auth error template

---
+++ src/utils/errors.ts
@@ -0,0 +1,12 @@
+export type ErrorCause = { statusCode: number; code: string; message: string };
+
+const httpError = (statusCode: number, code: string, message: string) =>
+  new Error(`${code}: ${message}`, {
+    cause: { statusCode, code, message } satisfies ErrorCause,
+  });
+
+export const badRequest = (c: string, m: string) => httpError(400, c, m);
+export const unauthorized = (c: string, m: string) => httpError(401, c, m);
+export const notFound = (c: string, m: string) => httpError(404, c, m);
+export const unprocessable = (c: string, m: string) => httpError(422, c, m);
+export const tooManyRequests = (c: string, m: string) => httpError(429, c, m);
